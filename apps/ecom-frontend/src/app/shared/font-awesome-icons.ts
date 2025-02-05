8ef9499b180a342fcf618ff32ccfba54e7e6ed44
dariuscruceru21 <dariuscruceru@gmail.com>
2025-02-06 00:26:41 +0200

    General ui added(footer and navbar)

--- apps/ecom-frontend/src/app/shared/font-awesome-icons.ts
+++ apps/ecom-frontend/src/app/shared/font-awesome-icons.ts
@@ -1,8 +1,14 @@
 import { IconDefinition } from '@fortawesome/angular-fontawesome';
-import { faUser } from '@fortawesome/free-solid-svg-icons';
+import { faCartShopping, faTruckFast, faUser } from '@fortawesome/free-solid-svg-icons';
+import { faFacebook, faTwitter, faYoutube } from '@fortawesome/free-brands-svg-icons';
 
 export const fontAwesomeIcons: IconDefinition[]=[
-  faUser
+  faUser,
+  faCartShopping,
+  faTruckFast,
+  faTwitter,
+  faYoutube,
+  faFacebook
 
 
 ];
